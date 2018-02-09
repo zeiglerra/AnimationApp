859a4e80aa0433f4aee568d40c434cb83c729b18
rzeigler3 <rzeigler3@csc.com>
2018-02-08 21:45:06 -0600

    Initial commit

---
+++ app/src/main/java/com/example/rick/animationapp/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.rick.animationapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RelativeLayout rlSlide = (RelativeLayout) findViewById(R.id.rlSlide);
+        final TextView txvTitle = (TextView) findViewById(R.id.txvTitle);
+
+        Animation myAnimation = AnimationUtils.loadAnimation(MainActivity.this, R.anim.spin_in);
+        rlSlide.setAnimation(myAnimation);
+
+        myAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+                Animation combo = AnimationUtils.loadAnimation(MainActivity.this, R.anim.fade_in);
+                txvTitle.setAnimation(combo);
+                txvTitle.setAlpha(1.0F);
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+}
